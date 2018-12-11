7de3ce0ca71d8ed8be9a8ed1b5140448fc2e6c78
Yotam Ramon <yotam@ethernitynet.com>
2018-12-11 12:31:57 -0500

    fix start script

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@ DPDK_IMG=${1:-local}
 
 
 case ${DPDK_IMG} in
-        "hub")
-        DPDK_IMG=shrewdthingsltd/docker-dpdk:dpdk-17.11.4
-        docker pull $OVS_IMG
-        ;;
-        *)
-        ./2.10.1/build_ovs.sh
-        OVS_IMG=local/docker-dpdk:dpdk-17.11.4
-        ;;
+	"hub")
+	DPDK_IMG=shrewdthingsltd/docker-dpdk:dpdk-17.11.4
+	docker pull $DPDK_IMG
+	;;
+	*)
+	./17.11.4/build_dpdk.sh
+	DPDK_IMG=local/docker-dpdk:dpdk-17.11.4
+	;;
 esac
 
 docker run -ti --net=host --privileged -v /mnt/huge:/mnt/huge --device=/dev/uio0:/dev/uio0 $DPDK_IMG /bin/bash
