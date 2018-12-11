5097aa9ce7d8ac4316b4222eddc493033d015466
Yotam Ramon <yotam@ethernitynet.com>
2018-12-11 13:10:32 -0500

    consolidate local build to start script

--- 17.11.4/build_dpdk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pushd ./17.11.4
-docker build -t local/docker-dpdk:dpdk-17.11.4 ./
-popd
--- start.sh
+++ start.sh
@@ -9,10 +9,20 @@ case ${DPDK_IMG} in
 	docker pull $DPDK_IMG
 	;;
 	*)
-	./17.11.4/build_dpdk.sh
 	DPDK_IMG=local/docker-dpdk:dpdk-17.11.4
+	cd ./17.11.4
+	docker build \
+		-t $DPDK_IMG \
+		./
+	cd -
 	;;
 esac
 
-docker run -ti --net=host --privileged -v /mnt/huge:/mnt/huge --device=/dev/uio0:/dev/uio0 $DPDK_IMG /bin/bash
-
+docker run \
+	-ti \
+	--net=host \
+	--privileged \
+	-v /mnt/huge:/mnt/huge \
+	--device=/dev/uio0:/dev/uio0 \
+	$DPDK_IMG \
+	/bin/bash
